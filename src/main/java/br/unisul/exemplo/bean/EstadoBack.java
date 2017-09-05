75dea123a48c3c1c7656a222f552454f49d19c1f
Tiago Boeing <tiagoboeing@bol.com.br>
2017-09-04 21:59:43 -0300

    Bug

--- src/main/java/br/unisul/exemplo/bean/EstadoBack.java
+++ src/main/java/br/unisul/exemplo/bean/EstadoBack.java
@@ -3,40 +3,75 @@ package br.unisul.exemplo.bean;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
+import br.unisul.exemplo.dao.EstadoDao;
 import br.unisul.exemplo.domain.Estado;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
+
 public class EstadoBack implements Serializable{
 
 	private Estado estado = new Estado();
 	private ArrayList<Estado> listaEstados = new ArrayList<Estado>();
 	
 	
+	@PostConstruct
+	public void listar() {
+		try {
+			
+			EstadoDao dao = new EstadoDao();
+			listaEstados = (ArrayList<Estado>)dao.listarTodos();
+			
+		} catch (Exception e) {
+			Messages.addGlobalInfo("Erro ao listar UFs");
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void editaEstado(ActionEvent evt){
 		estado = (Estado) evt.getComponent().getAttributes().get("ufAlterar");
+		
+		EstadoDao dao = new EstadoDao();
+		dao.alterar(estado);
+		listaEstados = (ArrayList<Estado>)dao.listarTodos();
 	}
 	
 	
 	public void excluiEstado(ActionEvent evt){ 
-		estado = (Estado) evt.getComponent().getAttributes().get("ufExcluir");
+//		estado = (Estado) evt.getComponent().getAttributes().get("ufExcluir");
+//		
+//		Messages.addGlobalInfo(estado.getNomeEstado() + "-" + estado.getUfEstado());
 		
-		Messages.addGlobalInfo(estado.getNomeEstado() + "-" + estado.getUfEstado());
+		estado = (Estado) evt.getComponent().getAttributes().get("ufExcluir");
+		EstadoDao dao = new EstadoDao();
+		dao.excluir(estado);
+		
+		listaEstados = (ArrayList<Estado>)dao.listarTodos();
+		//Messages.addGlobalInfo(estado.getNome() + "-" + estado.getUf());
 	}
 	
 	
 	public void cadastraEstado(){
 		
 		//System.out.println(estado.getNomeEstado() + "-" + estado.getUfEstado());
-		Messages.addGlobalInfo(estado.getNomeEstado() + "-" + estado.getUfEstado());
-		listaEstados.add(estado);
+//		Messages.addGlobalInfo(estado.getNomeEstado() + "-" + estado.getUfEstado());
+//		listaEstados.add(estado);
+//		estado = new Estado();
+		
+		EstadoDao dao = new EstadoDao();
+		dao.salvar(estado);
+		// System.out.println(estado.getNome() + " - " + estado.getUf());
+		Messages.addGlobalInfo(estado.getNomeEstado() + " - " + estado.getUfEstado());
+		listaEstados = (ArrayList<Estado>)dao.listarTodos();
 		estado = new Estado();
 		
 	}
